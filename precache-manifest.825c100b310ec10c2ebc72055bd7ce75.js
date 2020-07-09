bf2dc23edb84e2562d2d6170ef7ee7cf883267e6
Aditya Chavan <adityachavan677@gmail.com>
2020-07-09 21:16:14 +0530

    Updates

---
+++ precache-manifest.825c100b310ec10c2ebc72055bd7ce75.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b2f807e96cccaac8079c302b54a806e",
+    "url": "/MyCrudApp/index.html"
+  },
+  {
+    "revision": "1d5765098a4d8f0dd131",
+    "url": "/MyCrudApp/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "a005c28a9572c2a55803",
+    "url": "/MyCrudApp/static/css/main.1c238c75.chunk.css"
+  },
+  {
+    "revision": "1d5765098a4d8f0dd131",
+    "url": "/MyCrudApp/static/js/2.36604621.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MyCrudApp/static/js/2.36604621.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a005c28a9572c2a55803",
+    "url": "/MyCrudApp/static/js/main.26490349.chunk.js"
+  },
+  {
+    "revision": "4c6f69a8a3a284c2700b",
+    "url": "/MyCrudApp/static/js/runtime-main.c1b5f4e8.js"
+  }
+]);
